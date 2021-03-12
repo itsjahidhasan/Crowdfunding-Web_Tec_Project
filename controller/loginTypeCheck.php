3b21f06a22aafecc5026458440161a7251cfbd2a
Md. Jahid Hasan <mdjahidhasanf92@gmail.com>
2021-03-12 15:42:53 +0600

    Admin modified V 0.2

--- controller/loginTypeCheck.php
+++ controller/loginTypeCheck.php
@@ -1,13 +1,35 @@
 <?php 
 if(isset( $_POST['submit'])){
   
-  if(isset($_POST['radio'])){
+  
     $type = $_POST['loginType'];
+    
     if($type == "Admin"){
+      
       header('location: ../View/Admin/adminLogin.html');
      
     }
+    if($type == "Employee"){
+      
+      header('location: ../View/Admin/adminLogin.html');
+     
+    }
+    if($type == "Donner"){
+      
+      header('location: ../View/Admin/adminLogin.html');
+     
+    }
+    if($type == "Applicant"){
+      
+      header('location: ../View/Admin/adminLogin.html');
+     
+    }
+    else{
+      echo "You need select a option";
+    }
+
+    
   }
-}
+
 
 ?>
